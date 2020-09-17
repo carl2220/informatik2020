4fd13007c8b3b34d13790d5989081a50994d43a1
carl2220 <69799433+carl2220@users.noreply.github.com>
2020-09-17 08:28:54 +0200

    Add files via upload

---
+++ firstpic.js
@@ -0,0 +1,14 @@
+let img;
+
+function preload() {
+  img = loadImage("https: //postimg.cc/yWJZfHQm");
+  }
+
+      function setup() {
+        createCanvas(windowWidth, windowHeight);
+        loadImage("https: //postimg.cc/yWJZfHQm")
+      }
+
+      function draw() {
+
+      }
